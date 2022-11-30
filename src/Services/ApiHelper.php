4e55275004062f070987c8e47ebc05c82fa73add
sheriyar <sheheryar.saleem@smartlane.pk>
2022-11-30 18:56:19 +0500

    change composer.json and apihelper

--- src/Services/ApiHelper.php
+++ src/Services/ApiHelper.php
@@ -366,26 +366,63 @@ class ApiHelper implements IApiHelper
      */
     public function createWebhook(array $payload): ResponseAccess
     {
-        $query = '
-        mutation webhookSubscriptionCreate(
-            $topic: WebhookSubscriptionTopic!,
-            $webhookSubscription: WebhookSubscriptionInput!
-        ) {
-            webhookSubscriptionCreate(
-                topic: $topic
-                webhookSubscription: $webhookSubscription
-            ) {
-                userErrors {
-                    field
+//        $query = '
+//        mutation webhookSubscriptionCreate(
+//            $topic: WebhookSubscriptionTopic!,
+//            $webhookSubscription: WebhookSubscriptionInput!
+//        ) {
+//            webhookSubscriptionCreate(
+//                topic: $topic
+//                webhookSubscription: $webhookSubscription
+//            ) {
+//                userErrors {
+//                    field
+//                    message
+//                }
+//                webhookSubscription {
+//                    id
+//                    topic
+//                }
+//            }
+//        }
+//        ';
+        $address_type = Util::getShopifyConfig('webhook_address_type');
+        if($address_type === "arn"){
+            $query = '
+            mutation eventBridgeWebhookSubscriptionCreate($topic: WebhookSubscriptionTopic!, $webhookSubscription: EventBridgeWebhookSubscriptionInput!) {
+                eventBridgeWebhookSubscriptionCreate(topic: $topic, webhookSubscription: $webhookSubscription) {
+                  userErrors {
                     message
-                }
-                webhookSubscription {
+                  }
+                  webhookSubscription {
                     id
                     topic
+                  }
+                }
+              }
+              ';
+        }else{
+            $query = '
+            mutation webhookSubscriptionCreate(
+                $topic: WebhookSubscriptionTopic!,
+                $webhookSubscription: WebhookSubscriptionInput!
+            ) {
+                webhookSubscriptionCreate(
+                    topic: $topic
+                    webhookSubscription: $webhookSubscription
+                ) {
+                    userErrors {
+                        field
+                        message
+                    }
+                    webhookSubscription {
+                        id
+                        topic
+                    }
                 }
             }
+            ';
         }
-        ';
 
         // Change REST-format topics ("resource/event")
         // to GraphQL-format topics ("RESOURCE_EVENT"), for pre-v17 compatibility
@@ -393,7 +430,8 @@ class ApiHelper implements IApiHelper
         $variables = [
             'topic' => $topic,
             'webhookSubscription' => [
-                'callbackUrl' => $payload['address'],
+                //'callbackUrl' => $payload['address'],
+                $address_type => $payload['address'],
                 'format' => 'JSON',
             ],
         ];
